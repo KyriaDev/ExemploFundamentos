cc8cb3cbc09be3107ecbfb0f6dd909bd94d4af79
Kyria Letícia Lima De Almeida <kyrialeticia103@gmail.com>
2023-12-15 22:43:14 -0300

    Update Program.cs

--- EXEMPLO_FUNDAMENTOS/Program.cs
+++ EXEMPLO_FUNDAMENTOS/Program.cs
@@ -575,23 +575,55 @@ arrayInteiros[2] = 50;
 //Array.Resize(ref arrayInteiros, arrayInteiros.Length * 2);
 
 //Copiando um Array para outro e alterando o tamanho (nesse caso dobrando)
-int[] arrayInteirosDobrado = new int[arrayInteiros.Length * 2];
-Array.Copy(arrayInteiros, arrayInteirosDobrado, arrayInteiros.Length);
+//int[] arrayInteirosDobrado = new int[arrayInteiros.Length * 2];
+//Array.Copy(arrayInteiros, arrayInteirosDobrado, arrayInteiros.Length);
 
-Console.WriteLine("Percorrendo um Array com o FOR");
-for(int contador = 0; contador < arrayInteiros.Length; contador++)
-{
-    Console.WriteLine($"Posição N° {contador} = {arrayInteiros[contador]}");
-}
+//Console.WriteLine("Percorrendo um Array com o FOR");
+//for(int contador = 0; contador < arrayInteiros.Length; contador++)
+//{
+//    Console.WriteLine($"Posição N° {contador} = {arrayInteiros[contador]}");
+//}
 
 //TRABALHANDO COM LISTAS
+//Lista = "Array melhorado", um "Array menos complexo".
+//A lista possui muito mais métodos que o Array.
+//EX:Não é necessáio gerenciar o tamanho.
+//Sempre que possível utilize uma lista ao invés de uma Array.
+//Sintaxe de uma lista
+//List<string> listaString = new List<string>();
+//Adicionar elementos na lista
+//Adicionar um elemento
+// listaString.Add("SP");
+// listaString.Add("BA");
+// listaString.Add("MG");
+// listaString.Add("RJ");
 
+// Console.WriteLine($"Itens na minha lista: {listaString.Count} - Capacidade: {listaString.Capacity}");
 
+// listaString.Add("SC");
 
+// Console.WriteLine($"Itens na minha lista: {listaString.Count} - Capacidade: {listaString.Capacity}");
 
+// listaString.Remove("MG");
 
+// Console.WriteLine($"Itens na minha lista: {listaString.Count} - Capacidade: {listaString.Capacity}");
 
-
+//Percorrendo uma Lista
+//Utilizando o FOR
+// Console.WriteLine("Percorrendo a Lista com o FOR");
+// for(int contador = 0; contador < listaString.Count; contador++)
+// {
+//     Console.WriteLine($"Posição N° {contador} = {listaString[contador]}");
+// }
+//Utilizando o FOREACH
+// Console.WriteLine("Percorrendo a Lista com o FOREACH");
+// int contadorForeach = 0;
+// foreach(string item in listaString)
+// {
+//     Console.WriteLine($"Posição N° {contadorForeach} = {item}");
+//     contadorForeach++;
+// }
+//O FOREACH é o mais recomendado.
 
 
 
